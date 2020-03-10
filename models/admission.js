1f44389e566688e5b1ccacc90b1ef0f738b6f2ea
muhammadhusnain <chhassnain16@gmail.com>
2020-03-10 12:00:05 +0500

    complete schema

--- models/admission.js
+++ models/admission.js
@@ -1,20 +1,61 @@
 const mongoose= require('mongoose')
 
 const admissionSchema= new mongoose.Schema({
-  first_name:{
-    type:String,
-    required:true
-  },
-  middle_name:{
-    type:String,
-    required:true,
-    default:"DF"
-  },
-  last_name:{
-    type:String,
-    required:true
-  }
-
+  first_name:{    type:String,    required:true  },
+  middle_name:{ type:String,    required:true, },
+  last_name:{    type:String,    required:true  },
+  father_name:{    type:String,    required:true  },
+  ident_mark:{    type:String,    required:true,  },
+  gender:{    type:String,    required:true  },
+  dob:{type:String, required:true},
+  age:{type:String, required:true},
+  place_of_birth:{type:String, required:true},
+  nationality:{    type:String,    required:true,  },
+  cnic:{    type:String,    required:true  },
+  contact:{type:String, required:true},  
+  landline_contact:{    type:String,    required:true,  },
+  present_add:{    type:String,    required:true  },
+  permanent_add:{type:String, required:true},
+  mailing:{type:String, required:true},
+  email:{    type:String,    required:true,  },
+  is_on_camp:{    type:String,    required:true  },
+  domicile:{type:String, required:true},
+  religion:{type:String, required:true},
+  s1_name:{    type:String,    required:false,  },
+  s1_age:{    type:String,    required:false,  },
+  s1_institute:{    type:String,    required:false,  },
+  s1_grade:{    type:String,    required:false,  },
+  s2_name:{    type:String,    required:false,  },
+  s2_age:{    type:String,    required:false,  },
+  s2_institute:{    type:String,    required:false,  },
+  s2_grade:{    type:String,    required:false,  },
+  g_name:{    type:String,    required:true,  },
+  g_relation:{    type:String,    required:true  },
+  g_contact:{type:String, required:true},
+  g_email:{type:String, required:true},
+  g_offic_contact:{    type:String,    required:true,  },
+  g_designation:{    type:String,    required:true,  default:"Phy-Math-Comp Science"},
+  g_profession:{type:String, required:true},
+  g_annual_incom:{type:String, required:true},
+  urgent_name:{    type:String,    required:true,  },
+  urgent_relation:{    type:String,    required:true  },
+  urgent_cnic:{type:String, required:true},
+  urgent_contact:{type:String, required:true},
+  school_name:{type:String, required:true},
+  school_location:{    type:String,    required:true,  },
+  school_certificate:{    type:String,    required:true  },
+  school_board:{type:String, required:true},
+  subjects:{type:String, required:true},
+  ssc_1_marks:{    type:String,    required:true,  },
+  ssc_2_marks:{    type:String,    required:true,  },  
+  subject:{    type:String,    required:true,  },
+  ice_sub:{    type:String,    required:true,  default:"Phy-Math-Comp Science"},
+  form_b:{type:String, required:true},
+  matric_marksheet:{type:String, required:true},
+  father_cnic:{type:String, required:true},
+  nine_mark_sheet:{    type:String,    required:true,  },
+  leaving_certificate:{    type:String,    required:true,},
+  char_certificate:{type:String, required:true},
 })
 
 module.exports=mongoose.model('Admission',admissionSchema)
--- routes/admissions.js
+++ routes/admissions.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const Admission = require('../models/admission')
 
+// router.get('/',async(reeq,res)=>{
+//   alert('Hello World')
+// })
 //Getting All data
 router.get('/', async(req,res) => {
   try {
@@ -11,7 +14,6 @@ router.get('/', async(req,res) => {
     res.status(500).json({message:e.message})
   }
 })
-
 //Getting one data
 router.get('/:id',getAdmission , (req,res) =>{
   res.send(res.admission)
@@ -26,14 +28,64 @@ router.get('/name/:id',async (req,res) =>{
   }
 
 })
-
-
 //Saving a new Data
 router.post('/', async(req,res)=>{
   const admission = new Admission({
     first_name:req.body.first_name,
     middle_name:req.body.middle_name,
-    last_name:req.body.last_name 
+    last_name:req.body.last_name,
+    father_name:req.body.father_name,
+    ident_mark:req.body.ident_mark,
+    gender:req.body.gender,
+    dob:req.body.dob,
+    age:req.body.age,
+    place_of_birth:req.body.place_of_birth,
+    nationality:req.body.nationality,
+    cnic:req.body.cnic,
+    contact:req.body.docontactb,  
+    landline_contact:req.body.landline_contact,
+    present_add:req.body.present_add,
+    permanent_add:req.body.permanent_add,
+    mailing:req.body.mailing,
+    email:req.body.email,
+    is_on_camp:req.body.is_on_camp,
+    domicile:req.body.domicile,
+    religion:req.body.religion,  
+    s1_name:req.body.s1_name,
+    s1_age:req.body.s1_age,
+    s1_institute:req.body.s1_institute,
+    s1_grade:req.body.s1_grade,
+    s2_name:req.body.s2_name,
+    s2_age:req.body.s2_age,
+    s2_institute:req.body.s2_institute,
+    s2_grade:req.body.s2_grade,
+    g_name:req.body.g_name,
+    g_relation:req.body.g_relation,
+    g_contact:req.body.g_contact,
+    g_email:req.body.g_email,
+    g_offic_contact:req.body.g_offic_contact,
+    g_designation:req.body.g_profession,
+    g_profession:req.body.g_profession,
+    g_annual_incom:req.body.g_annual_incom,
+    urgent_name:req.body.urgent_name,
+    urgent_relation:req.body.urgent_relation,
+    urgent_cnic:req.body.urgent_cnic,
+    urgent_contact:req.body.urgent_contact,
+    school_name:req.body.school_name,
+    school_location:req.school_location,
+    school_certificate:req.body.school_certificate,
+    school_board:req.body.school_board,
+    subjects:req.body.subjects,
+    ssc_1_marks:req.body.ssc_1_marks,
+    ssc_2_marks:req.body.ssc_2_marks, 
+    subject:req.body.subjects,
+    ice_sub:req.body.ice_sub,
+    form_b:req.body.form_b,
+    matric_marksheet:req.body.matric_marksheet,
+    father_cnic:req.body.father_cnic,
+    nine_mark_sheet:req.body.nine_mark_sheet,
+    leaving_certificate:req.body.leaving_certificate,
+    char_certificate:req.body.char_certificate,
   })
   try {
     const newadmission = await admission.save()
@@ -61,9 +113,6 @@ router.patch('/:id' , getAdmission, async(req,res)=>{
   }
 })
 //Deleting a record
-
-
-
 async function getAdmission(req,res,next){
     let admission
     try {
@@ -77,5 +126,4 @@ async function getAdmission(req,res,next){
     res.admission=admission
     next()
 }
-
 module.exports = router
